df1ea9f4df76eb99ce4d1e6a1937e33e49a6fd91
Mikhailov Sergey <sergeypolet@yandex.ru>
2020-05-16 19:16:44 +0300

    init project

---
+++ MultiplyModules/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiplyModules
+//
+//  Created by Sergey Mikhailov on 16.05.2020.
+//  Copyright © 2020 Sergey Mikhailov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
