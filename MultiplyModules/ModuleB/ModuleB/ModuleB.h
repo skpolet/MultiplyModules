de5fad4b713a04cc0a1d7245da17da2df3f8eb64
Mikhailov Sergey <sergeypolet@yandex.ru>
2020-05-16 20:07:29 +0300

    add Module B

---
+++ MultiplyModules/ModuleB/ModuleB/ModuleB.h
@@ -0,0 +1,19 @@
+//
+//  ModuleB.h
+//  ModuleB
+//
+//  Created by Sergey Mikhailov on 16.05.2020.
+//  Copyright © 2020 Sergey Mikhailov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModuleB.
+FOUNDATION_EXPORT double ModuleBVersionNumber;
+
+//! Project version string for ModuleB.
+FOUNDATION_EXPORT const unsigned char ModuleBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleB/PublicHeader.h>
+
+
