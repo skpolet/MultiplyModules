589514c17ec87668fa20908b9f1b66f18ea7498b
Mikhailov Sergey <sergeypolet@yandex.ru>
2020-05-16 20:04:36 +0300

    add module A

---
+++ MultiplyModules/ModuleA/ModuleA/ModuleA.h
@@ -0,0 +1,19 @@
+//
+//  ModuleA.h
+//  ModuleA
+//
+//  Created by Sergey Mikhailov on 16.05.2020.
+//  Copyright © 2020 Sergey Mikhailov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModuleA.
+FOUNDATION_EXPORT double ModuleAVersionNumber;
+
+//! Project version string for ModuleA.
+FOUNDATION_EXPORT const unsigned char ModuleAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleA/PublicHeader.h>
+
+
